9c3ff19b3a2758bc958b431486f7db9841c9fd8a
mitchell <mitchell2001b@gmail.com>
2022-04-08 12:10:53 +0200

    backend stuff for tasks

---
+++ src/test/java/com/example/demoo/task/TaskControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.demoo.task;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+    @Test
+    void getAllTasks() {
+    }
+}
