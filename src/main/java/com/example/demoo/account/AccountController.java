097bdc01731c8d5c9412a2a60286890cb6c05ed4
mitchell <mitchell2001b@gmail.com>
2022-05-12 17:21:42 +0200

    Added unit test with h2 and starting to add docker compose stuff

--- src/main/java/com/example/demoo/account/AccountController.java
+++ src/main/java/com/example/demoo/account/AccountController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5000"})
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:5000", "http://localhost:8181"})
 @RestController
 @RequestMapping(path = "api/v1/account")
 public class AccountController
